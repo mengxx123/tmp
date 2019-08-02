de9810b7448fdd2bbd7ff2804fd21b65f36d38da
ydying <1778679036@qq.com>
2019-08-02 18:47:19 +0800

    backup
    Signed-off-by: ydying <1778679036@qq.com>

---
+++ python/test.py
@@ -0,0 +1,11 @@
+# print('hello world')
+
+a = 'Hello'
+print(a)
+
+user = {'name': 'CJH'}
+
+user.name = '123'
+
+print(user.name)
+
---
+++ test.js
@@ -0,0 +1,11 @@
+let a = 0
+
+
+
+console.log('12312121', '2121')
+console.log('测试')
+console.log('测试')
+
+console.log('测试')
+
+vars
